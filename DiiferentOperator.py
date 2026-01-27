acf277e4b6a029736505081250f9913a8f598985
Priyanshu Tiwari <2023cspriyanshu14454@poornima.edu.in>
2026-01-27 22:44:32 +0530

    Add files via upload

---
+++ DiiferentOperator.py
@@ -0,0 +1,45 @@
+print(-10//3)
+
+
+#Assignment Operator----------
+
+x=5
+
+x+=3
+print(x)
+
+x-=3
+print(x)
+
+x*=3
+print(x)
+
+x/=3
+print(x)
+
+x %= 3
+print(x)
+
+x //= 3
+print(x)
+
+x**=3
+print(x)
+
+x &= 3
+print(x)
+
+x |= 3
+print(x)
+
+x ^= 3
+print(x)
+
+x >>= 3
+print(x)
+
+x <<= 3
+print(x)
+
+
+
